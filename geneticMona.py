9971470158810c6284e2ba592d647fba8280b931
Aaron Hill <aaron_hill2@carleton.ca>
2020-10-26 01:47:07 -0400

    render chain fully functional

--- geneticMona.py
+++ geneticMona.py
@@ -15,6 +15,55 @@ from PIL import Image
 
 import pdb
 
+triangle_shader = {
+    "vert": """
+#version 450
+layout(location = 0) in vec4 position;
+layout(location = 1) in vec4 color;
+out vec4 color_out;
+void main() {
+    gl_Position = vec4(position.xyz * 2 - 1, 1);
+    color_out = color;
+    color_out.w = 1.0;
+}
+""",
+    "frag": """
+#version 450
+smooth in vec4 color_out;
+out vec4 outputColor;
+void main() {
+    outputColor = color_out;
+    outputColor.w = 1.0;
+}
+""",
+}
+
+ssd_shader = {
+    "vert": """
+#version 450
+layout(location = 0) in vec4 position;
+void main() {
+    gl_Position = vec4(position.xyz * 2 - 1, 1);
+}
+""",
+    "frag": """
+#version 450
+layout(location = 0) uniform vec2 resolution;
+layout(location = 1) uniform sampler2D frame_texture;
+layout(location = 2) uniform sampler2D image_texture;
+out vec4 outputColor;
+void main() {
+    vec2 uv  = gl_FragCoord.xy / resolution;
+    vec4 image = texture(image_texture, uv);
+    vec4 frame = texture(frame_texture, uv);
+    outputColor += 0.0 * pow(image - frame, vec4(2.0));
+    outputColor = (image - frame);
+    outputColor *= outputColor;
+    outputColor.w = 1.0;
+}
+""",
+}
+
 vert_shader = """
 #version 450
 layout(location = 0) in vec4 position;
@@ -24,7 +73,7 @@ out vec4 color_out;
 
 void main()
 {
-   gl_Position = vec4(position.xyz *2 - 1, 1);
+   gl_Position = vec4(position.xyz * 2 - 1, 1);
    color_out = color;
    color_out.w = 1.0;
 }
@@ -33,17 +82,18 @@ void main()
 frag_shader = """
 #version 450
 layout(location = 0) uniform sampler2D image;
-layout(location = 1) uniform vec2 resolution;
-layout(location = 2) uniform int draw_diff;
+layout(location = 1) uniform sampler2D framebuffer;
+layout(location = 2) uniform vec2 resolution;
+layout(location = 3) uniform int draw_diff;
 smooth in vec4 color_out;
 
 out vec4 outputColor;
 
 void main()
 {
-    outputColor = color_out - texture(image, gl_FragCoord.xy / resolution) * draw_diff;
+    outputColor = color_out - texture(image, gl_FragCoord.xy / resolution);
     outputColor.w = 1.0;
-    outputColor *= outputColor;
+    //outputColor *= outputColor;
 }
 """
 
@@ -78,12 +128,13 @@ class Controller():
 
     def __init__(self, image=None, scale=1):
 
-        self.population_size = 30
+        self.population_size = 12
         self.elite_pool_size = 1
-        self.mutation_rate   = 0.0005
-        self.mutation_amount = 0.05
+        self.random_pool_size = 2
+        self.mutation_rate   = 0.001
+        self.mutation_amount = 0.01
 
-        self.num_tris = 50
+        self.num_tris = 30
         self.tris_per_pop = self.num_tris * self.population_size
         self.verts_per_pop = self.tris_per_pop * 3
         self.floats_per_pop = self.verts_per_pop * 6    # (2 for position, 4 for RGBA)
@@ -99,6 +150,7 @@ class Controller():
         self.w = self.img.width
         self.h = self.img.height
         self.frame_count = 0
+        self.gen_count = 0
         self.best_gene = 0
         self.best_score = 1000000000000
 
@@ -107,6 +159,16 @@ class Controller():
         self.shader = shaders.compileProgram(
             shaders.compileShader(vert_shader, gl.GL_VERTEX_SHADER),
             shaders.compileShader(frag_shader, gl.GL_FRAGMENT_SHADER)
+        )
+        # shader to draw triangles to framebuffer
+        self.triangle_shader = shaders.compileProgram(
+            shaders.compileShader(triangle_shader["vert"], gl.GL_VERTEX_SHADER),
+            shaders.compileShader(triangle_shader["frag"], gl.GL_FRAGMENT_SHADER)
+        )
+        # shader to draw triangles to framebuffer
+        self.ssd_shader = shaders.compileProgram(
+            shaders.compileShader(ssd_shader["vert"], gl.GL_VERTEX_SHADER),
+            shaders.compileShader(ssd_shader["frag"], gl.GL_FRAGMENT_SHADER)
         )
         # shader to copy offscreen framebuffer to main framebuffer
         self.copy_shader = shaders.compileProgram(
@@ -117,22 +179,29 @@ class Controller():
         # shader attribute indices
         self.pos_id = gl.glGetAttribLocation(self.shader, b'position')
         self.col_id = gl.glGetAttribLocation(self.shader, b'color')
-        self.img_id = gl.glGetUniformLocation(self.shader, b'image')
-        self.res_id = gl.glGetUniformLocation(self.shader, b"resolution")
-        self.dif_id = gl.glGetUniformLocation(self.shader, b"draw_diff")
+        self.image_id = gl.glGetUniformLocation(self.ssd_shader, b'image_texture')
+        self.frame_id = gl.glGetUniformLocation(self.ssd_shader, b'frame_texture')
+        self.res_id = gl.glGetUniformLocation(self.ssd_shader, b"resolution")
 
         # create texture
         self.texture_id = gl.glGenTextures(1)
 
         # create framebuffer for off-screen rendering
         self.frame_buffer = None
-        self._init_texture()
-        self._init_frame_buffer()
+        #self._init_texture()
+        #self._init_frame_buffer()
+        self._init_buffers()
         self._init_vertex_buffers()
         #self._init_image()
 
         self._init_gl()
         self.t0 = time.time()
+
+    def _get_image_data(self):
+        # TODO: RGB instead of RGBA, maybe float datatype?
+        img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
+        img = img.convert("RGBA")
+        return np.array(list(img.getdata()), dtype=np.uint8)
 
     def _init_texture(self):
         img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
@@ -170,6 +239,46 @@ class Controller():
             print("Error initializing framebuffer")
             exit()
 
+        gl.glViewport(0, 0, self.w, self.h)
+
+    def _init_buffers(self):
+        self.image_texture   = gl.glGenTextures(1)     # texture storing input image
+        self.frame_buffer    = gl.glGenFramebuffers(1) # framebuffer for offscreen render
+        self.frame_buffer2   = gl.glGenFramebuffers(1) # second offscreen buffer for ping-ponging
+        self.render_texture  = gl.glGenTextures(1)     # texture for storing offscreen render
+        self.render_texture2 = gl.glGenTextures(1)     # second offscreen texture for ping-ponging
+        
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.image_texture)
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, self.w, self.h, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, self._get_image_data())
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
+
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.render_texture)
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, self.w, self.h, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, ctypes.c_void_p(0))
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
+
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.render_texture2)
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, self.w, self.h, 0, gl.GL_RGBA, gl.GL_UNSIGNED_BYTE, ctypes.c_void_p(0))
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
+
+        # attach texture to currently bound framebuffer
+        gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.frame_buffer)
+        gl.glFramebufferTexture2D(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, self.render_texture, 0)
+
+        # attach texture to currently bound framebuffer
+        gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.frame_buffer2)
+        gl.glFramebufferTexture2D(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0, gl.GL_TEXTURE_2D, self.render_texture2, 0)
+
+        status = gl.glCheckFramebufferStatus(gl.GL_FRAMEBUFFER)
+        if status != gl.GL_FRAMEBUFFER_COMPLETE:
+            print("Error initializing framebuffer")
+            exit()
+        
         gl.glViewport(0, 0, self.w, self.h)
 
     def _init_vertex_buffers(self):
@@ -212,38 +321,54 @@ class Controller():
 
     def _get_fitness(self, index):
         gl.glViewport(0, 0, self.w, self.h)
+
         gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.frame_buffer)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
-        gl.glUseProgram(self.shader)
+        # draw triangles to framebuffer
+        gl.glUseProgram(self.triangle_shader)
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vert_data_buffer)
         gl.glEnableVertexAttribArray(self.pos_id)
         gl.glVertexAttribPointer(0, 2, gl.GL_FLOAT, False, 0, ctypes.c_void_p(0))
         gl.glEnableVertexAttribArray(self.col_id)
         gl.glVertexAttribPointer(1, 4, gl.GL_FLOAT, False, 0, ctypes.c_void_p(0))
 
-        gl.glBindTexture(gl.GL_TEXTURE_2D, self.image_texture)
-        gl.glActiveTexture(gl.GL_TEXTURE0)
-        gl.glUniform1i(self.img_id, 0)
-        gl.glUniform1i(self.dif_id, 1)
-        gl.glUniform2f(self.res_id, self.w, self.h)
-
         gl.glDrawArrays(gl.GL_TRIANGLES, index * self.verts_per_gene, self.verts_per_gene)
         gl.glDisableVertexAttribArray(self.col_id)
         gl.glDisableVertexAttribArray(self.pos_id)
+        gl.glUseProgram(0) # TODO: remove
+
+        gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.frame_buffer2)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+        gl.glUseProgram(self.ssd_shader)
+        gl.glActiveTexture(gl.GL_TEXTURE0)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.render_texture)
+        gl.glActiveTexture(gl.GL_TEXTURE1)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, self.image_texture)
+        gl.glUniform1i(self.frame_id, 0)
+        gl.glUniform1i(self.image_id, 1)
+        gl.glUniform2f(self.res_id, self.w, self.h)
+
+        # TODO: use vertex array
+        #gl.glBindVertexArray(self.quad_vao)
+        gl.glBegin(gl.GL_QUADS)
+        gl.glVertex4f(0, 0, 0, 1)
+        gl.glVertex4f(0, 1, 0, 1)
+        gl.glVertex4f(1, 1, 0, 1)
+        gl.glVertex4f(1, 0, 0, 1)
+        gl.glEnd()
+        #gl.glDrawArrays(gl.GL_TRIANGLES, 0, 6)
         gl.glUseProgram(0)
 
-        # gl.glBindFramebuffer(gl.GL_READ_FRAMEBUFFER, self.frame_buffer)
-        # gl.glBindFramebuffer(gl.GL_DRAW_FRAMEBUFFER, 0)
-        # gl.glClearColor(0, 0, 0, 1)
-        # gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        # gl.glBlitFramebuffer(0, 0, self.w, self.h, 0, 0, self.w, self.h, gl.GL_COLOR_BUFFER_BIT, gl.GL_NEAREST)
+        #gl.glBlitNamedFramebuffer(self.frame_buffer2, 0, 0, 0, self.w, self.h, 0, 0, self.w, self.h, gl.GL_COLOR_BUFFER_BIT, gl.GL_NEAREST)
 
         gl.glFlush()
 
         gl.glReadBuffer(gl.GL_COLOR_ATTACHMENT0)
         gl.glPixelStorei(gl.GL_PACK_ALIGNMENT, 1)
-        data = gl.glReadPixels (0, 0, self.w, self.h, gl.GL_RGB,  gl.GL_UNSIGNED_BYTE)
+        # TODO: try floats?
+        data = gl.glReadPixels (0, 0, self.w, self.h, gl.GL_RGB,  gl.GL_FLOAT)
         fitness = np.frombuffer(data, dtype=np.uint8).sum()
 
         gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
@@ -253,9 +378,12 @@ class Controller():
     def _mutate(self, index):
         """Apply mutations to the gene specified by index"""
         best = self.vert_data[self.best_gene * self.floats_per_gene:(self.best_gene + 1) * self.floats_per_gene]
-        mutation = (np.random.rand(self.floats_per_gene).astype(np.float32) - 1.0) * 2 * self.mutation_amount * 0.01 # random between +/- mutation amount
+        mutation = (np.random.rand(self.floats_per_gene).astype(np.float32) - 0.5) * 2 * self.mutation_amount# random between +/- mutation amount
         mutation *= np.random.rand(self.floats_per_gene) > self.mutation_rate # multiply by mutation mask
         self.vert_data[index*self.floats_per_gene:(index+1)*self.floats_per_gene] = best + mutation
+
+    def _randomize(self, index):
+        self.vert_data[index * self.floats_per_gene:(index + 1) * self.floats_per_gene] = np.random.rand(self.floats_per_gene)
 
     def step(self):
         # get fitness scores for population
@@ -269,49 +397,45 @@ class Controller():
             glut.glutPostRedisplay()
 
         # mutate the peasants
-        for i in range(self.elite_pool_size, self.population_size):
+        for i in range(self.elite_pool_size, self.population_size - self.random_pool_size):
             self._mutate(fitness_scores[i][0])
+        for i in range(self.population_size - self.random_pool_size, self.population_size):
+            self._randomize(fitness_scores[i][0])
         self.vert_data = np.clip(self.vert_data, 0, 1)
 
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vert_data_buffer)
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self.vert_data.nbytes, (gl.GLfloat * self.vert_data.size)(*self.vert_data), gl.GL_DYNAMIC_DRAW)
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
 
+        self.gen_count += 1
+        if self.gen_count % 10 == 0:
+            glut.glutSetWindowTitle(u"glMona - {}".format(self.gen_count / (time.time() - self.t0)))
+
 
     def draw(self):
         gl.glViewport(0, 0, self.w, self.h)
 
-        gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.frame_buffer)
+        gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
-        gl.glUseProgram(self.shader)
+        # draw triangles to framebuffer
+        gl.glUseProgram(self.triangle_shader)
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.vert_data_buffer)
         gl.glEnableVertexAttribArray(self.pos_id)
         gl.glVertexAttribPointer(0, 2, gl.GL_FLOAT, False, 0, ctypes.c_void_p(0))
         gl.glEnableVertexAttribArray(self.col_id)
         gl.glVertexAttribPointer(1, 4, gl.GL_FLOAT, False, 0, ctypes.c_void_p(0))
 
-        gl.glBindTexture(gl.GL_TEXTURE_2D, self.image_texture)
-        gl.glActiveTexture(gl.GL_TEXTURE0)
-        gl.glUniform1i(self.img_id, 0)
-        gl.glUniform1i(self.dif_id, 0)
-        gl.glUniform2f(self.res_id, self.w, self.h)
-
         gl.glDrawArrays(gl.GL_TRIANGLES, self.best_gene * self.verts_per_gene, self.verts_per_gene)
         gl.glDisableVertexAttribArray(self.col_id)
         gl.glDisableVertexAttribArray(self.pos_id)
         gl.glUseProgram(0)
 
-        #gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
-        #gl.glBindFramebuffer(gl.GL_READ_FRAMEBUFFER, self.frame_buffer)
-        #gl.glBindFramebuffer(gl.GL_DRAW_FRAMEBUFFER, 0)
-        #gl.glClearColor(0, 0, 0, 1)
-        #gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        gl.glBlitNamedFramebuffer(self.frame_buffer, 0, 0, 0, self.w, self.h, 0, 0, self.w, self.h, gl.GL_COLOR_BUFFER_BIT, gl.GL_NEAREST)
-
         gl.glFlush()
 
         self.frame_count += 1
+        #if self.frame_count % 1000 == 0:
+        #    glut.glutSetWindowTitle(u"glMona - {}".format(self.frame_count / (time.time() - self.t0)))
 
         # float colors python sum 83.90426205358702
         # float colors numpy sum  399.39343067326814
